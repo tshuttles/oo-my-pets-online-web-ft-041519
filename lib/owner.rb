e0c266ac3cb1994f3b705c53e53d70bfa1507fae
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-07 16:12:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name, :pets 
+  attr_reader :species
+  
+  @@all = [] 
+  
+  def initialize(name) 
+    @name = name 
+    @species = species 
+    @pets = {fishes: [], cats: [], dogs: []} 
+    @@all << self 
+  end 
+  
+  def species 
+    @species = species 
+  end 
+  
+  def say_species 
+  end 
+  
+  def buy_cat(name) 
+    cat_name = Cat.new(name) 
+  end 
+  
+  def buy_dog(name)
+    dog_name = Dog.new(name)
+  end 
+  
+  def buy_fish(name) 
+    fish_name = Fish.new(name) 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+  end 
+  
+  def self.reset_all 
+  end 
 end
