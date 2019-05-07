d3846c7f582cf81dcb127758c0100280ab8c044a
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-07 18:08:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,7 @@ class Owner
   end 
   
   def list_pets 
-    "I have #{:fishes.length} fish, #{:dogs.length} dog"
+    "I have #{:fishes.length} fish, #{:dogs.length} dog(s), and #{:cats.length} cat(s)."
   end 
   
 end
